eeb7cf62d4e8979936ce25f88236ff693bbfe07c
Marjan Hosseini <45966768+Marjan-Hosseini@users.noreply.github.com>
2024-01-04 14:56:31 -0500

    update

--- data/input_handler.py
+++ data/input_handler.py
@@ -113,13 +113,13 @@ class InputHandler:
         command = [prefix, os.path.join(root_dir, "extract_poly/build/extractHAIRS"), "--bam", bam_filename,
                    "--vcf", vcf_filename, "--out", out_filename]
         print("Executing command:", ' '.join(command))
-        subprocess.run(['ls', os.path.join(root_dir, "extract_poly/build")])
-        subprocess.run(['ls', '/home/FCAM/mhosseini/HaplOrbit/output/'])
-        subprocess.run(['cat', '/home/FCAM/mhosseini/HaplOrbit/output/test.txt'])
-        subprocess.run(['mkdir', os.path.join(output_dir, 'test2')])
+        # subprocess.run(['ls', os.path.join(root_dir, "extract_poly/build")])
+        # subprocess.run(['ls', '/home/FCAM/mhosseini/HaplOrbit/output/'])
+        # subprocess.run(['cat', '/home/FCAM/mhosseini/HaplOrbit/output/test.txt'])
+        # subprocess.run(['mkdir', os.path.join(output_dir, 'test2')])
 
-        print("Executing command:", ' '.join(command))
-        subprocess.run(command)
+        # print("Executing command:", ' '.join(command))
+        subprocess.check_call(command)
 
 
         # subprocess.check_call(
