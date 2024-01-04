8b49d7d879f9a2e7494157c6c01959f748bc45d4
Marjan Hosseini <45966768+Marjan-Hosseini@users.noreply.github.com>
2024-01-04 14:22:42 -0500

    update

--- data/input_handler.py
+++ data/input_handler.py
@@ -113,7 +113,7 @@ class InputHandler:
         command = [prefix, os.path.join(root_dir, "extract_poly/build/extractHAIRS"), "--bam", bam_filename,
                    "--vcf", vcf_filename, "--out", out_filename]
         print("Executing command:", ' '.join(command))
-        # subprocess.check_call(command)
+        subprocess.check_output(command)
         
         # subprocess.check_call(
         #     [prefix, root_dir + "/extract-poly/build/extractHAIRS", "--bam", bam_filename, "--vcf", vcf_filename,
