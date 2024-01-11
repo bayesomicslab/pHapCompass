8cb363b32c081d4fa7185684360f9c728db6a446
Marjan Hosseini <45966768+Marjan-Hosseini@users.noreply.github.com>
2024-01-11 18:49:27 -0500

    data path

--- data/configuration.py
+++ data/configuration.py
@@ -12,7 +12,7 @@ class Configuration:
         self.epsilon = epsilon
         self.dynamic_config = {}
         self.global_phasings, self.global_likelihoods = self.initialization()
-        print(self.global_likelihoods)
+        print('conf2', self.global_likelihoods[2])
 
     def set_algorithm_type(self, algorithm_type):
         self.algorithm_type = algorithm_type
