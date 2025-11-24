09a483f35746b507df720ee7e817ac9f9ee23476
Marjan Hosseini <marjan.hosseini777@gmail.com>
2025-11-23 19:18:36 -0500

    Clean

--- algorithm/haplotype_assembly.py
+++
@@ -1,9 +0,0 @@
-class HaplotypeAssembly:
-    def __init__(self, ploidy):
-        self.ploidy = ploidy
-        # Other initialization...
-
-    def initialize_assembly(self):
-        # Implement initial computations or setup
-        pass
-
